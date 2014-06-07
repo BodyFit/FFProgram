05e386f56bc3fb4e0166188908ffcca18a64e17c
miromiroslavov <miroslav.miroslavov@gmail.com>
2014-06-07 20:31:53 +0300

    Detailed program for program 1.

--- server/api/program.js
+++ server/api/program.js
@@ -3,26 +3,51 @@ var async = require("async"),
   profileManager = require("../profileManager");
 
 exports.init = function (swagger) {
-  var getProgram = {
+  var getProgramDetails = {
     "spec": {
-      "description": "Operations about prototypes",
-      "path": "/programs/{program}",
-      "notes": "Returns a prototype based on ID",
-      "summary": "Find prototype by ID",
+      "description": "Get the detailed Program for the current user",
+      "path": "/programs/{program}/details",
+      "notes": "Get the detailed Program for the current user",
+      "summary": "Get the detailed Program for the current user",
       "method": "GET",
       "params": [swagger.pathParam("id", "ID of prototype that needs to be fetched", "string")],
       "responseClass": "Prototype",
-      "errorResponses": [swagger.errors.invalid("id"), swagger.errors.notFound("prototype")],
-      "nickname": "getPrototypeById"
+      "errorResponses": [swagger.errors.invalid("id")],
+      "nickname": "getDetailedProgram"
     },
     "action": function (req, res) {
       var program = req.params.program - 1;
       var userId = req.user.Id;
+
+      profileManager.getProfile(userId, req.headers.authorization, function (profile) {
+        profileManager.getBiometrics(userId, req.headers.authorization, function (biom) {
+          res.json(pm.getDetailedProgram(program, profile, biom[biom.length - 1]));
+        });
+      });
+    }
+  };
+
+  var getProgram = {
+    "spec": {
+      "description": "Get Program for the current user.",
+      "path": "/programs/{program}",
+      "notes": "Get Program for the current user.",
+      "summary": "Get Program for the current user.",
+      "method": "GET",
+      "params": [swagger.pathParam("id", "ID of prototype that needs to be fetched", "string")],
+      "responseClass": "Program",
+      "errorResponses": [swagger.errors.invalid("id"), swagger.errors.notFound("prototype")],
+      "nickname": "getProgram"
+    },
+    "action": function (req, res) {
+      var program = req.params.program - 1;
+      var userId = req.user.Id;
+
       profileManager.getBiometrics(userId, req.headers.authorization, function (biom) {
         res.json(pm.getProgram(program, biom[biom.length - 1]));
       });
     }
-  }
+  };
 
   var getGoals = {
     "spec": {
@@ -39,8 +64,9 @@ exports.init = function (swagger) {
     "action": function (req, res) {
       res.json(pm.goals);
     }
-  }
+  };
 
   swagger.addGet(getProgram);
+  swagger.addGet(getProgramDetails);
   swagger.addGet(getGoals);
 };
--- server/programManager.js
+++ server/programManager.js
@@ -27,10 +27,10 @@ function getFat(f) {
   }
 }
 
-function getProgram(id, profile) {
-  var program = programs[id],
+function getProgram(goal, biometrics) {
+  var program = programs[goal],
     general = program.general,
-    weight = profile.weight,
+    weight = biometrics.weight,
     fatPercents = getFat(general.f),
     carbs = expand(general.c, weight),
     proteins = expand(general.p, weight),
@@ -44,6 +44,59 @@ function getProgram(id, profile) {
     proteins: proteins,
     fats: fats
   };
+}
+
+function getOption(options, training) {
+  return options[0];
+}
+
+function getDetailedProgram(goal, profile, biometrics) {
+  var nutrients = getProgram(goal, biometrics),
+    details = programs[goal].details,
+    trainings = profile.trainings,
+    options = details.options,
+    days = [],
+    day,
+    carbs, prots, fats,
+    nut,
+    i, j,
+    training,
+    option,
+    meals, meal;
+
+
+  for (i = 0; i < 7; i++) {
+    carbs = nutrients.carbs[i];
+    prots = nutrients.proteins[i];
+    fats = nutrients.fats[i];
+    training = _.find(trainings, function (item) {
+      return item.day === i;
+    });
+    option = getOption(options, training);
+    meals = option.meals;
+    day = [];
+    if(training) {
+      training.isTraining = true;
+      delete training.day;
+      day.push(training);
+    }
+    for (j = 0; j < meals.length; j++) {
+      meal = meals[j];
+      day.push({
+        from: meal.time[0],
+        to: meal.time[1],
+        carbs: Math.round((meal.nutrients.c || 0) * carbs),
+        proteins: Math.round((meal.nutrients.p || 0) * prots),
+        fats: Math.round((meal.nutrients.f || 0) * fats)
+      })
+    }
+    day = _.sortBy(day, function(item){
+      return item.from;
+    })
+    days.push(day);
+  }
+
+  return days;
 }
 
 function readGoals() {
@@ -64,3 +117,4 @@ exports.init = readGoals;
 exports.goals = goals;
 exports.programs = programs;
 exports.getProgram = getProgram;
+exports.getDetailedProgram = getDetailedProgram;
